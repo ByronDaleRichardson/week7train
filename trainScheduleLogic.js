5a53e00eff411f121bf33ea6e5e5f707e25a6a03
ByronDaleRichardson <musicmanx@gmail.com>
2017-01-26 18:31:28 -0500

    figured out minutes til

--- trainScheduleLogic.js
+++ trainScheduleLogic.js
@@ -78,5 +78,5 @@ database.ref().on("child_added", function(childSnapshot, prevChildKey) {
   var nextTrain = moment().add(tTillTrain, "minutes");
 
   // Add each train's data into the table
-  $("#train-table > tbody").append("<tr><td>" + trainName + "</td><td>" + trainDest + "</td><td>" + trainFrequency + "</td><td>" + moment(nextTrain).format("hh:mm A") + "</td><td>" + "" + "</td></tr>");
+  $("#train-table > tbody").append("<tr><td>" + trainName + "</td><td>" + trainDest + "</td><td>" + trainFrequency + "</td><td>" + moment(nextTrain).format("hh:mm A") + "</td><td>" + tTillTrain + "</td></tr>");
 });
